21ed8ba88ac3615bb2a9876b968f5cb40eeb016a
Chukwuemeka <eimuoh@gmail.com>
2023-03-11 17:12:54 +0100

    modified the hamburger menu list

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 document.querySelector('.hamburger-menu').addEventListener('click', ()=>{
     document.querySelector('.navigation').classList.toggle('change')
-});
+})
+
+
+let y = document.getElementsByClassName('toggle-profile');
+for(let i = 0; i < y.length; i++){
+    y[i].addEventListener('click', ()=>{
+    document.querySelector('.list-container').classList.toggle('reveal')
+})}
+
+let x = document.getElementsByClassName('toggle-archive');
+for(let i = 0; i < y.length; i++){
+    x[i].addEventListener('click', ()=>{
+    document.querySelector('.list-container').classList.toggle('show')
+})}
+
